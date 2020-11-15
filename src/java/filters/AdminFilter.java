9d9c848fe42fd863e1f3c564b764e22d5332e8c1
832465 <832465@SAIT225779.ACDM.DS.SAIT.CA>
2020-11-15 14:17:44 -0700

    Added admin filter

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -12,6 +12,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 
@@ -25,6 +28,17 @@ public class AdminFilter implements Filter {
             throws IOException, ServletException {
         
         //code to execute before the servlet or next filter
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        
+        if(!email.contains("admin")) {
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+                
+        
        
          chain.doFilter(request, response); //execute servlet or next filter
             
